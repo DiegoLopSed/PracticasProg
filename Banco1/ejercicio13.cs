157ac8bdb9c48f25f46b906d8c2d65584f41b4ad
Diego Lopez <diegolopsed160703@gmail.com>
2024-10-21 23:55:16 -0600

    Ejercicio 13 banco 1

---
+++ Banco1/ejercicio13.cs
@@ -0,0 +1,11 @@
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+
+        for (int i = 0; i <= 100; i++) {
+            Console.WriteLine("Número: "+i);
+        }
+
+    }
+}
