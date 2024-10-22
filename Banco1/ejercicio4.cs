eaf2d6b8b7bf918f813bab590d27a13831704aae
Diego Lopez <diegolopsed160703@gmail.com>
2024-10-21 23:23:30 -0600

    Ejercicio 4 banco 1

---
+++ Banco1/ejercicio4.cs
@@ -0,0 +1,12 @@
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        for (int i = 1; i < 50; i++) {
+            if (i % 2 != 0)
+            {
+                Console.WriteLine("Número inpar : " + i);
+            }
+        }
+    }
+}
